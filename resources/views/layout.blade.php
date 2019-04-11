5e33ee3b04ee329a924cc9fac162d03d15ec8818
Russsstle <rjohn.sants@yahoo.com>
2019-04-11 12:34:33 +0800

    title dynamic

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,9 @@
   <meta charset="UTF-8">
   <meta name="csrf-token" content=" {{ csrf_token() }}">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>{{ setting("title") }}</title>
+  <title>@php $url=explode("/",$_SERVER["REQUEST_URI"]);
+          echo ucwords(end($url))
+         @endphp</title>
   <link rel="stylesheet" href="{{ mix("css/app.css") }}">
 </head>
 <body>
